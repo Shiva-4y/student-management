f741ac59bebc9ff668583b46b905ac240601413d
sanoria <akashikun14@yahoo.com>
2024-11-09 17:59:47 +0800

    Activity 2 - Install preline and display students

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    
+
+        return view('welcome');
+
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    
+
+
 });
 
 require __DIR__.'/auth.php';
