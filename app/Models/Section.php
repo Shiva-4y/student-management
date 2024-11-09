4a9ec0597b54bcaafa1c7362f1fb0e636d509488
sanoria <akashikun14@yahoo.com>
2024-11-09 16:32:38 +0800

    Activity 1 - Install livewire, create models, migration and seeder

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Section extends Model
 {
-    /** @use HasFactory<\Database\Factories\SectionFactory> */
+   
     use HasFactory;
+
+    protected $fillable = [
+        'class_id', 
+        'name'
+    ];
+    public function class()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+  
 }
